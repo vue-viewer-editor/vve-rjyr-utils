2eb4f9032a90c3b9eb9d7e4884c155add1bb7e56
zhengjinmda <903355860@qq.com>
2019-08-26 18:06:06 +0800

    check

---
+++ src/IpCheck.ts
@@ -0,0 +1,4 @@
+export function ipCheck(ip: string): boolean {
+  const reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+  return reg.test(ip)
+}
