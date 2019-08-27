fa68f9b0c7b0f7cd6dd9350e7e1022d3625580f6
zhengjinmda <903355860@qq.com>
2019-08-27 10:13:12 +0800

    check

--- test/test.js
+++ test/test.js
@@ -2,58 +2,64 @@ var expect = require('expect.js');
 require('jsdom-global')() // for cookie
 
 // ts 测试编译后文件
-var base = require('../src/index.ts');
-var macCheck = require('../src/macCheck.ts')
-var ipCheck = require('../src/ipCheck.ts')
-var maskCheck = require('../src/maskCheck.ts')
-var wlanCheck = require('../src/wlanCheck.ts')
-var CookieProcess = require('../src/cookieProcess.ts')
+var {macCheck, ipCheck, maskCheck, wlanCheck, setCookie, getCookie, removeCookie} = require('../src/index.ts');
 
 describe('单元测试', function () {
   //mac
   describe('测试 mac 地址是否合法', function () {
     it('mac 地址正确', function () {
-      expect(macCheck.macCheck('54-BF-64-5C-E8-5F')).to.equal(true)
-    });
+      expect(macCheck('54-BF-64-5C-E8-5F')).to.equal(true)
+    })
     it('mac 地址错误', function () {
-      expect(macCheck.macCheck('54-BF-64-5C')).to.equal(false)
-    });
+      expect(macCheck('54-BF-64-5C')).to.equal(false)
+    })
     it('mac 地址错误', function () {
-      expect(macCheck.macCheck('54-BF-64-5C')).to.equal(false)
-    });
-  });
+      expect(macCheck('54-BF-64-5C')).to.equal(false)
+    })
+  })
   //ip
   describe('测试 ip 地址是否合法', function () {
     it('ip 地址正确', function () {
-      expect(ipCheck.ipCheck('172.30.81.82')).to.equal(true)
-    });
+      expect(ipCheck('172.30.81.82')).to.equal(true)
+    })
     it('ip 地址错误', function () {
-      expect(ipCheck.ipCheck('256.0.0.0')).to.equal(false)
-    });
-  });
+      expect(ipCheck('256.0.0.0')).to.equal(false)
+    })
+  })
   //掩码
   describe('测试掩码是否合法', function () {
     it('掩码正确', function () {
-      expect(maskCheck.maskCheck('255.255.255.0')).to.equal(true)
-    });
+      expect(maskCheck('255.255.255.0')).to.equal(true)
+    })
     it('掩码错误', function () {
-      expect(maskCheck.maskCheck('256.0.0.0')).to.equal(false)
-    });
-  });
+      expect(maskCheck('256.0.0.0')).to.equal(false)
+    })
+  })
   //wlan 是否含有中文
   describe('测试 wlan 是否含有中文', function () {
     it('不含中文', function () {
-      expect(wlanCheck.wlanCheck('ruijie')).to.equal(true)
-    });
+      expect(wlanCheck('ruijie')).to.equal(true)
+    })
     it('含中文', function () {
-      expect(wlanCheck.wlanCheck('锐捷ruijie123')).to.equal(false)
-    });
+      expect(wlanCheck('锐捷ruijie123')).to.equal(false)
+    })
   });
   // 测试 cookie
   describe('测试 cookie', function () {
-    it('设置cookie', function () {
-      CookieProcess.setCookie('name', 'cookie')
-      expect(CookieProcess.getCookie('name')).to.equal('cookie')
-    });
-  });
-});
+    it(' 设置cookie ', function () {
+      setCookie('name', 'cookie')
+      expect(getCookie('name')).to.equal('cookie')
+    })
+    it(' 设置cookie ', function () {
+      setCookie('name2', 'cookie2')
+      expect(getCookie('name2')).to.equal('cookie2')
+    })
+    it('获取全部 cookie ', function() {
+      expect(JSON.stringify(getCookie())).to.equal(JSON.stringify({name: "cookie", name2: "cookie2"}))
+    })
+    it('删除 cookie ', function() {
+      removeCookie('name2')
+      expect(JSON.stringify(getCookie())).to.equal(JSON.stringify({name: "cookie"}))
+    })
+  })
+})
